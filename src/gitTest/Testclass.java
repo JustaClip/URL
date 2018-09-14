8a4ca32823eeacd242c971e02b04e3c09c0431ba
Sebastien malmberg <43003643+JustaClip@users.noreply.github.com>
2018-09-14 15:17:21 +0200

    message

--- src/gitTest/Testclass.java
+++ src/gitTest/Testclass.java
@@ -1,10 +1,13 @@
 package gitTest;
+import java.util.Scanner;
 
 public class Testclass
 {
    public static void main(String[] args)
    {
-      
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("what is your name?");
+      String name = keyboard.nextLine();S
    }
 
 }
